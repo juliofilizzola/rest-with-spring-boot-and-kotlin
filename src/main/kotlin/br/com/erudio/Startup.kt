31082a0225d7d63dc512682aa8e74801d07fb5a5
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-09-25 15:55:44 -0300

    feat/count

--- src/main/kotlin/br/com/erudio/Startup.kt
+++ src/main/kotlin/br/com/erudio/Startup.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class RestWithSpringBootAndKotlinApplication
+class Startup
 
 fun main(args: Array<String>) {
-	runApplication<RestWithSpringBootAndKotlinApplication>(*args)
+    runApplication<Startup>(*args)
 }
