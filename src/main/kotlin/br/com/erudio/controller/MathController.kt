50f9f93a37bec5118af5fffce2a43fbb82362807
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-09-30 11:11:02 -0300

    feat: create package controller

--- src/main/kotlin/br/com/erudio/controller/MathController.kt
+++ src/main/kotlin/br/com/erudio/controller/MathController.kt
@@ -1,5 +1,6 @@
-package br.com.erudio
+package br.com.erudio.controller
 
+import br.com.erudio.service.MathService
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
